58ea1cb45324ab472095f91980dbe09e6039983e
hanzhonghao <hanzhonghao593513235@gmail.com>
2019-05-23 00:03:38 +0800

    add mine

--- pages/userLogin/login.js
+++ pages/userLogin/login.js
@@ -55,22 +55,13 @@ Page({
               icon: 'success',
               duration: 2000
             });
-            // app.userInfo = res.data.data;
-            // fixme 修改原有的全局对象为本地缓存
-            app.setGlobalUserInfo(res.data.data);
+             app.userInfo = res.data.data;
+
             // 页面跳转
 
-            var redirectUrl = me.redirectUrl;
-            if (redirectUrl != null && redirectUrl != undefined && redirectUrl != '') {
-              wx.redirectTo({
-                url: redirectUrl,
+            wx.redirectTo({
+              url: '../mine/mine'
               })
-            } else {
-              wx.redirectTo({
-                url: '../mine/mine',
-              })
-            }
-            
           } else if (res.data.status == 500) {
             // 失败弹出框
             wx.showToast({
